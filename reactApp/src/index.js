e51c53b9de61d12369349caa0a2b18aa05f23983
randallfine <randallfine@gmail.com>
2017-02-21 18:45:49 -0600

    updated files

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -1,3 +1,27 @@
-console.log("Works?!?")
-const root = document.querySelector('#root')
-root.innerHTML = '<p>Hello BLAH.</p>';
+import React from "react";
+import { render } from "react-dom";
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    render() {
+        return (
+          <div className ="container">
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">
+                <Header homeLink="Home"/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">
+                <Home name={"Max"} initialAge={38}/>
+              </div>
+            </div>
+          </div>
+        );
+    }
+}
+
+
+render(<App/>, window.document.getElementById("app"));
